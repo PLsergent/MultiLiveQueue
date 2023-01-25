015faf4e37d32717bda46c970ed36f5de1a62588
Pierre-Louis Sergent <papa.louis59@gmail.com>
2023-01-25 19:12:18 +0100

    Improve leaderboard by adding player global rank

--- ranking/RankingCommands.py
+++ ranking/RankingCommands.py
@@ -13,14 +13,25 @@ class Rank(app_commands.Group):
     @app_commands.command(name="leaderboard", description="View the leaderboard")
     async def leaderboard(self, ctx):
         leaderboard = self.ranks.get_leaderboard()
-        embed = Embed(title="Leaderboard 🔥", description="Top 10 players", color=0x64e4f5)
+        embed = Embed(title="Leaderboard 🏆", description="Top 10 players", color=0x64e4f5)
         file = File("./assets/Marvin.png")
         embed.set_thumbnail(url="attachment://Marvin.png")
         for i, player in enumerate(leaderboard):
             user = UserController(player)
             embed.add_field(name=f"{i + 1}. {player}", value=f"Rank: {user.ranking}\nPoints: {user.ranking_points}", inline=False)
+        user_leaderboard = self.ranks.get_player_global_ranking(ctx.user.name + "#" + ctx.user.discriminator)
+        embed.set_footer(text=f"Your global rank: n° {user_leaderboard + 1}")
         await ctx.response.send_message(file=file, embed=embed)
 
     @app_commands.command(name="me", description="Get your rank")
     async def me(self, ctx):
-        await ctx.response.send_message(self.ranks.get_my_rank(ctx.user.name + "#" + ctx.user.discriminator))
+        username = ctx.user.name + "#" + ctx.user.discriminator
+        user_leaderboard = self.ranks.get_player_global_ranking(username)
+        user = UserController(username)
+        embed = Embed(title=f"{username} 🏆", color=0x64e4f5)
+        embed.add_field(name="Rank", value=user.ranking, inline=True)
+        embed.add_field(name="Points", value=user.ranking_points, inline=True)
+        embed.add_field(name="Global rank", value=f"n° {user_leaderboard}", inline=False)
+        file = File("./assets/Marvin.png")
+        embed.set_thumbnail(url="attachment://Marvin.png")
+        await ctx.response.send_message(file=file, embed=embed)
--- ranking/RankingController.py
+++ ranking/RankingController.py
@@ -18,10 +18,24 @@ class RankingController:
         leaderboard.sort(key=lambda x: UserController(x).ranking_points, reverse=True)
         return leaderboard[:11]
     
+    def get_full_leaderboard(self):
+        with open(PATH + "ranks.json", "r") as f:
+            ranks = json.load(f)
+        leaderboard = []
+        for _, item in ranks.items():
+            leaderboard.extend(item["players"])
+        leaderboard.sort(key=lambda x: UserController(x).ranking_points, reverse=True)
+        return leaderboard
+    
     def get_rank(self, rank):
         with open(PATH + "ranks.json", "r") as f:
             ranks = json.load(f)
         return ranks[rank]
+    
+    def get_player_global_ranking(self, username):
+        user = UserController(username)
+        leaderboard = self.get_full_leaderboard()
+        return leaderboard.index(user.username)
     
     def get_my_rank(self, username):
         user = UserController(username)
